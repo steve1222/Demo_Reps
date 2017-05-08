5b0e5ffb15a5a150bea7059aedff249472573a45
UnrealPC_Steve\Steve <orangutangballs@gmail.com>
2017-05-08 14:26:05 -0400

    refactored basic detection code of physics object

--- Testing_Grounds/Source/Testing_Grounds/Grabber.cpp
+++ Testing_Grounds/Source/Testing_Grounds/Grabber.cpp
@@ -21,42 +21,94 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	GetPhysicsHandle();
+	BindActionToGrab();
+	//code will now detect if grab action is pressed, then call grab function
 	
 }
+/// look for attached physics handle
+void UGrabber::GetPhysicsHandle() {
+	//
+	physxHandlerComp = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 
+	//blah
+	if (physxHandlerComp) {
+		UE_LOG(LogTemp, Warning, TEXT("%s: Grabber Component found."), *GetOwner()->GetName())
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("%s: %s"), *GetOwner()->GetName(), *FString("I didnt find a physics component, meng."))
+	}
+}
+
+/// look for attached input component (only appears at run time)
+void UGrabber::BindActionToGrab()
+{
+	ownerInputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if (ownerInputComponent) {
+		UE_LOG(LogTemp, Warning, TEXT("%s: Input Component found."), *GetOwner()->GetName())
+			// bind actions
+		ownerInputComponent->BindAction("Grab Penis", IE_Pressed, this, &UGrabber::GrabPressed);
+		ownerInputComponent->BindAction("Grab Penis", IE_Released, this, &UGrabber::GrabReleased);
+	}
+	//blah
+	else {
+		UE_LOG(LogTemp, Error, TEXT("%s: %s"), *GetOwner()->GetName(), *FString("I didnt find an input component, meng."))
+	}
+}
+
+
+
+void UGrabber::GrabPressed()
+{
+	FindPhysicsObjectinRange();
+	UE_LOG(LogTemp, Warning, TEXT("%s: GRAB PENIS PRESSED!!."), *GetOwner()->GetName())
+}
+
+const FHitResult UGrabber::FindPhysicsObjectinRange()
+{
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *PlayerViewPointRotation.ToString())
+	//	UE_LOG(LogTemp, Warning, TEXT("%s"), *(PlayerViewPointRotation.Vector()).ToString())
+	//	UE_LOG(LogTemp, Warning, TEXT("  "))
+	// ...
+	FVector PlayerDirection = PlayerViewPointRotation.Vector();
+	FVector LineTraceEnd = (PlayerViewPointLocation + PlayerDirection*Reach);
+	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 1.0f, 50.0f);
+	FHitResult Hit;
+	FCollisionQueryParams MyTraceParams(FName(TEXT("ASS MONKEY PARAMS")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		MyTraceParams);
+
+
+	if (Hit.GetActor() != nullptr) {
+		AActor* HitActor = Hit.GetActor();
+		FString ActorName = HitActor->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("HIT ACTOR: %s"), *ActorName);
+	}
+
+	return FHitResult();
+}
+
+void UGrabber::GrabReleased()
+{
+	UE_LOG(LogTemp, Error, TEXT("%s: GRAB PENIS RELEASED!!."), *GetOwner()->GetName())
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	//get ass
-	FVector PlayerViewPointLocation;
-	FRotator PlayerViewPointRotation;
 
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-		OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
-	FCollisionQueryParams MyTraceParams(FName(TEXT("ASS MONKEY PARAMS")), false, GetOwner());
-	//UE_LOG(LogTemp, Warning, TEXT("%s"), *PlayerViewPointRotation.ToString())
-	//	UE_LOG(LogTemp, Warning, TEXT("%s"), *(PlayerViewPointRotation.Vector()).ToString())
-	//	UE_LOG(LogTemp, Warning, TEXT("  "))
-		// ...
-		FVector PlayerDirection = PlayerViewPointRotation.Vector();
-		FVector LineTraceEnd = (PlayerViewPointLocation + PlayerDirection*Reach);
-		DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 1.0f, 50.0f);
-		FHitResult Hit;
-		GetWorld()->LineTraceSingleByObjectType(OUT Hit,
-			PlayerViewPointLocation,
-			LineTraceEnd,
-			FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
-			MyTraceParams);
-		if (Hit.GetActor() != nullptr) {
-			AActor* HitActor = Hit.GetActor();
-			FString ActorName = HitActor->GetName();
-			UE_LOG(LogTemp, Warning, TEXT("HIT ACTOR: %s"), *ActorName);
-		}
-		
 			
 		
 }
--- Testing_Grounds/Source/Testing_Grounds/Grabber.h
+++ Testing_Grounds/Source/Testing_Grounds/Grabber.h
@@ -16,15 +16,30 @@ public:
 	UGrabber();
 	UPROPERTY(EditAnywhere)
 		float Reach = 1000.0f;
+	// ray-cast and grab whats in reach
+	void GrabPressed();
+	void GrabReleased();
+
+	
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
+	// get the physics handle we added to the pawn
+	void GetPhysicsHandle();
+	//setup (assumed) attached input component
+	void BindActionToGrab();
+	//return hit result after finding a physics component
+	const FHitResult FindPhysicsObjectinRange();
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+	
+	UPhysicsHandleComponent* physxHandlerComp = nullptr;
+	UInputComponent* ownerInputComponent;
+	AActor* HitActor = nullptr;
+	FString ActorName;
 	
 };
+
+
